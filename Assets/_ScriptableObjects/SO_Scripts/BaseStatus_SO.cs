441ca8c73168eb05375bed54b527588834461c92
teeyou <teeyou1127@gmail.com>
2026-03-18 15:55:06 +0900

    [Feat] 플레이어 몬스터 탐색
    플레이어 스포너, 베이스 스텟 SO, 플레이어 탐색

---
+++ Assets/_ScriptableObjects/SO_Scripts/BaseStatus_SO.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/Data/Character Status Data", fileName = "CharacterData_")]
+public class BaseStatus_SO : ScriptableObject
+{
+    [SerializeField] private string _id;
+    [SerializeField] private string _chName;
+    [SerializeField] private int _defaultMaxHp;
+    [SerializeField] private int _defaultAtk;
+    [SerializeField] private int _defaultDef;
+    [SerializeField] private float _range;
+    [SerializeField] private GameObject _prefab;
+
+    public string Id { get { return _id; } }
+    public string ChName { get { return _chName; } }
+    public int DefaultMaxHp { get { return _defaultMaxHp; } }
+    public int DefaultAtk { get { return _defaultAtk; } }
+    public int DefaultDef { get { return _defaultDef; } }
+    public float Range { get { return _range; } }
+    public GameObject Prefab { get { return _prefab; } }
+
+}
--- Assets/_Scripts/Base/Unit.cs
+++ Assets/_Scripts/Base/Unit.cs
@@ -2,6 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 보류
+// BattleScene 에서 위치
+// Front, Middle -> Front or Middle 중 가까운 것 먼저 공격
+// Rear -> Rear 먼저 공격. Rear 없으면, 위치 상관없이 가장 가까운 것 먼저
+
+
+
+
+// Front, Middle, Rear -> 제일 가까운 것 먼저 공격
+public enum EPositionLine
+{
+    None,
+    Front,
+    Middle,
+    Rear,
+}
+
+public enum ECombatMap
+{
+    Field,
+    Battle,
+}
 
 public abstract class Unit : MonoBehaviour, IDamageable
 {
@@ -10,8 +32,17 @@ public abstract class Unit : MonoBehaviour, IDamageable
     protected int _atk;
     protected int _def;
     protected int _cp;
-    protected int _totalDamaged;
-    protected float _lifetime;
+
+    protected ECombatMap _map;      // 필드씬 , 배틀씬
+
+    protected EPositionLine _line;  // 배치 위치
+    protected float _range;         // 공격 가능 거리
+
+    protected int _totalDamage;     // 내가 준 피해량
+    protected int _totalDamaged;    // 내가 받은 피해량
+    protected float _lifetime;      // 전투 중 살아남은 시간
+    
+    protected Transform _targetTr;  // 공격 대상 (몬스터 -> 플레이어, 플레이어 -> 몬스터)
     
     public virtual void Attack()
     {
@@ -37,12 +68,12 @@ public abstract class Unit : MonoBehaviour, IDamageable
         
     }
 
-    public void SetMaxHp(int maxHp)
-    {
-        _maxHp = maxHp;
-    }
+    //public void SetMaxHp(int maxHp)
+    //{
+    //    _maxHp = maxHp;
+    //}
 
-    public int GetMaxHp() => _maxHp;
+    //public int GetMaxHp() => _maxHp;
 
 
 }
