1b0187eaf86ce7d58d3c5360e04908e4459e29e3
최지훈 <gnsl9746@gmail.com>
2026-03-19 11:42:26 +0900

    [feat] 더미 플레이어 제작, 몬스터 애니메이션 적용

---
+++ Assets/_Scripts/Enemy/DummyTarget.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyTarget : Unit
+{
+    public override void Attack()
+    {
+
+    }
+
+    public override void Skill()
+    {
+
+    }
+
+    public override void Heal(int value)
+    {
+
+    }
+
+    public override void TakeDamage(int damage, Transform target)
+    {
+        int finalDamage = Mathf.Max(1, damage - _def);
+        _curHp -= finalDamage;
+
+        Debug.Log($"{name}이 피해 받음 / 남은 HP : {_curHp}");
+
+        if (_curHp <= 0)
+        {
+            _curHp = 0;
+            Die();
+        }
+    }
+
+    public override void Die()
+    {
+        if (_isDead)
+            return;
+        _isDead = true;
+
+        Debug.Log($"{name} 죽음");
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        _maxHp = 100;
+        _curHp = _maxHp;
+        _def = 0;
+    }
+
+    void Update()
+    {
+        
+    }
+}
