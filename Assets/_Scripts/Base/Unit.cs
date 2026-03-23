34e63c5166be08b87ef4b4925a0a48a883a502e6
jjim-ozschool <jjim.ozschool@gmail.com>
2026-03-23 16:52:11 +0900

    [Feat] UI 디테일 작업2

---
+++ Assets/_Scripts/Base/Unit.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 보류
+// BattleScene 에서 위치
+// Front, Middle -> Front or Middle 중 가까운 것 먼저 공격
+// Rear -> Rear 먼저 공격. Rear 없으면, 위치 상관없이 가장 가까운 것 먼저
+
+
+
+
+// Front, Middle, Rear -> 제일 가까운 것 먼저 공격
+public enum EPositionLine
+{
+    None,
+    Front,
+    Middle,
+    Rear,
+}
+
+// 현재 씬이 필드씬 or 배틀씬
+public enum ECombatMap
+{
+    Field,
+    Battle,
+}
+
+// 근거리 공격 or 원거리 공격
+public enum EAttackType
+{
+    Melee,
+    Range,
+}
+
+public abstract class Unit : MonoBehaviour, IDamageable
+{
+    protected float _moveSpeed;
+    protected int _curHp;
+    protected int _maxHp;
+    protected int _atk;
+    protected int _def;
+    protected int _cp;
+    protected bool _isAttack;           // 공격중인지 확인
+
+    protected EAttackType _attackType; // 근거리 또는 원거리
+
+    protected ECombatMap _map;      // 필드씬 , 배틀씬
+
+    protected EPositionLine _line;  // 배치 위치
+    protected float _range;         // 공격 가능 거리
+
+    protected int _totalDamage;     // 내가 준 피해량
+    protected int _totalDamaged;    // 내가 받은 피해량
+    protected float _lifetime;      // 전투 중 살아남은 시간
+    
+    protected Transform _targetTr;  // 공격 대상 (몬스터 -> 플레이어, 플레이어 -> 몬스터)
+
+    protected bool _isDead;
+    public bool IsDead {get { return _isDead; } set { _isDead = value; } }
+
+    public float MoveSpeed { get { return _moveSpeed; } set { _moveSpeed = value; } }
+    public int CurHp { get { return _curHp; } set { _curHp = value; } }
+    public int MaxHp { get { return _maxHp; } set { _maxHp = value; } }
+    public int Atk { get { return _atk; } set { _atk = value; } }
+    public int Def { get { return _def; } set { _def = value; } }
+    public int Cp { get { return _cp; } set { _cp = value; } }
+    public bool IsAttack { get { return _isAttack; } set { _isAttack = value; } }
+
+    public EAttackType AttackType { get { return _attackType; } set { _attackType = value; } }
+    public ECombatMap Map { get { return _map; } set { _map = value; } }
+    public EPositionLine Line { get { return _line; } set { _line = value; } }
+    public float Range { get { return _range; } set { _range = value; } }
+    public int TotalDamage { get { return _totalDamage; } set { _totalDamage = value; } }
+    public int TotalDamaged { get { return _totalDamaged; } set { _totalDamaged = value; } }
+    public float LifeTime { get { return _lifetime; } set { _lifetime = value; } }
+    public Transform TargetTr { get { return _targetTr; } set { _targetTr = value; } }
+
+
+    public abstract void Attack();
+
+    public abstract void Skill();
+
+    public virtual void Heal(int value)
+    {
+
+    }
+
+    public virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+    public virtual void TakeDamage(int damage, Transform target)
+    {
+        
+    }
+}
