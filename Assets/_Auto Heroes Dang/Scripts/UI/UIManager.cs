14ef43b9b794255388a812f235a6c8d2e8e04366
teeyou <teeyou1127@gmail.com>
2026-03-30 10:53:22 +0900

    [Refactor] 불필요한 코드 제거

--- Assets/_Auto Heroes Dang/Scripts/UI/UIManager.cs
+++ Assets/_Auto Heroes Dang/Scripts/UI/UIManager.cs
@@ -1,7 +1,3 @@
-using Cinemachine;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -44,46 +40,6 @@ public class UIManager : Singleton<UIManager>
 
     private void LoadBattleScene(EGameStage stage)
     {
-        // 스테이지 진입하기 전에 현재 위치 정보 저장
-
-        //// 메인 캐릭터 저장
-        //DataSource.Instance.MainCharacterPosRot = new PosRotData(
-        //    FieldManager.Instance.MainCharacterTr.position, 
-        //    FieldManager.Instance.MainCharacterTr.rotation);
-
-        //// 서브 캐릭터 저장
-        //IReadOnlyList<PosRotData> list = DataSource.Instance.GetSubPosRotList();    // 필드 씬 위치정보 리스트
-
-        //IReadOnlyList<Transform> trList = FieldManager.Instance.GetSubCharacterTrList;  //필드 씬 트랜스폼 
-        //int len = trList.Count;
-
-        //// 위치정보 없으면 Add
-        //if (list.Count == 0)
-        //{
-        //    for (int i = 0; i < len; i++)
-        //    {
-        //        DataSource.Instance.AddSubPosRot(trList[i].position, trList[i].rotation);
-        //    }
-        //}
-
-        //// 위치정보 있으면 덮어쓰기
-        //else
-        //{
-        //    for (int i = 0; i < len; i++)
-        //    {
-        //        DataSource.Instance.SetSubPosRot(i, trList[i].position, trList[i].rotation);
-        //    }
-        //}
-        
-        //CinemachineDollyCart cart = FindObjectOfType<CinemachineDollyCart>();
-
-        //if (cart == null)
-        //{
-        //    Debug.Log("UIManager - cart NULL");
-        //}
-
-        //DataSource.Instance.CartPosition = cart.m_Position;
-
         FieldManager.Instance.IsSpawnPossible = false; // 스테이지로 씬 전환 전에 스폰 불가능하도록 막음
 
         SceneLoader.Instance.LoadScene(stage);
@@ -97,7 +53,6 @@ public class UIManager : Singleton<UIManager>
 
     public void CloseStagePanel()
     {
-        Debug.Log("CloseStagePanel");
         _fieldUI.ToggleStagePanel();
         ToggleStageButton(true);
     }
