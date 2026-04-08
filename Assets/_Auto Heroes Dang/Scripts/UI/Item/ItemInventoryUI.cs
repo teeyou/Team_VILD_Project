6d305ffbb41928366f1563bec9f7b7b9566acb42
jjim-ozschool <jjim.ozschool@gmail.com>
2026-04-08 12:51:48 +0900

    [Feat]인벤토리UI 패널 추가

--- Assets/_Auto Heroes Dang/Scripts/UI/Item/ItemInventoryUI.cs
+++ Assets/_Auto Heroes Dang/Scripts/UI/Item/ItemInventoryUI.cs
@@ -7,6 +7,10 @@ public class ItemInventoryUI : MonoBehaviour
     [SerializeField] private ItemSlotFactory _factory;
     [SerializeField] private int _verticalSlot = 5;
 
+    [SerializeField] private GameObject _shopPanel;
+    [SerializeField] private GameObject _inventoryPanel;
+    [SerializeField] private GameObject _enhancementPanel;
+
     private void OnEnable()
     {
         InventoryManager.Instance.OnChanged += Refresh;
@@ -32,6 +36,16 @@ public class ItemInventoryUI : MonoBehaviour
     {
         Debug.Log($"인벤토리 클릭: {item.name}");
         // if else로 상점이면 아이템 정보 표기, 인벤토리면 장착
+        if (_shopPanel.activeSelf)
+        {
+        }
+        else if (_inventoryPanel.activeSelf)
+        {
+        }
+        else if (_enhancementPanel.activeSelf)
+        {
+        }
+
 
     }
 }
