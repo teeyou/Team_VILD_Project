1286e9e7834ca608a44d6517313e4236eba9ead8
jjim-ozschool <jjim.ozschool@gmail.com>
2026-04-14 18:38:27 +0900

    [Chore] 아이템 설명 수정
    포션 탭

--- Assets/_Auto Heroes Dang/Scripts/Singleton/Inventory And Shop/ShopManagerPotion.cs
+++ Assets/_Auto Heroes Dang/Scripts/Singleton/Inventory And Shop/ShopManagerPotion.cs
@@ -64,8 +64,8 @@ public class ShopManagerPotion : Singleton<ShopManagerPotion>, IItemManage
     {
         _items.Clear();
 
-        ItemData atkItem = new ItemData("고기", ItemType.AtkBuff, Grade.AtkBuff, 5, 10, 1000, "힘이 세지는 고기");
-        ItemData defItem = new ItemData("풀", ItemType.DefBuff, Grade.DefBuff, 1, 10, 1000, "방어가 강해지는 풀");
+        ItemData atkItem = new ItemData("고기", ItemType.AtkBuff, Grade.AtkBuff, 5, 10, 1000, "힘이 세지는 고기\n영구 적용");
+        ItemData defItem = new ItemData("풀", ItemType.DefBuff, Grade.DefBuff, 1, 10, 1000, "방어가 강해지는 풀\n영구 적용");
 
         if (DataSource.Instance.atkPotionOn)
             atkItem.state = ItemState.SoldOut;
